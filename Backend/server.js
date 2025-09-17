2192deb35daef860727e3b30b04cc0f2908bec7e
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-18 00:20:25 +0530

    Database Connection from env file and port too

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+
+dotenv.config();
+import { connectDB } from "./config/db.js";
 
 // app config
 const app = express();
@@ -8,6 +12,9 @@ const port = process.env.PORT || 4000;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// db Connection
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Api Working");
