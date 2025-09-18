4afc0700731106424121e4e719222b3f683c3d06
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-18 19:04:39 +0530

    add api end point for foodlist

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -3,11 +3,11 @@ import fs from "fs";
 
 // Add food items
 const addFood = async (req, res) => {
-  if (!req.file) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Image file is required." });
-  }
+  // if (!req.file) {
+  //   return res
+  //     .status(400)
+  //     .json({ success: false, message: "Image file is required." });
+  // }
   let image_filename = `${req.file.filename}`;
 
   const food = new FoodModel({
@@ -27,4 +27,15 @@ const addFood = async (req, res) => {
   }
 };
 
-export { addFood };
+// all foodlist
+const listFood = async (req, res) => {
+  try {
+    const foods = await FoodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFood };
