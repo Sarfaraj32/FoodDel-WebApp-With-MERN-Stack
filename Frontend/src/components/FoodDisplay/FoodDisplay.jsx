75ba690b62f121a76b8a3ee9a4c986f180d7cbe6
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-20 19:47:04 +0530

    render all fooditem list from backend and some wrong file changes

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
-import { StoreContext } from "../../context/storeContext";
+import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
@@ -8,10 +8,10 @@ const FoodDisplay = ({ category }) => {
 
   return (
     <div className="food-display" id="food-display">
-      <h2>To dishes near you</h2>
+      <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          if ((category === "All" || category === item.category)) {
+          if (category === "All" || category === item.category) {
             return (
               <FoodItem
                 key={index}
