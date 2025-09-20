8b57886e650c671d801f00dffeefb5b3cd992696
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-20 19:49:36 +0530

    show image in cart from backend too

--- Frontend/src/pages/Card/Cart.jsx
+++ Frontend/src/pages/Card/Cart.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import "./Cart.css";
-import { StoreContext } from "../../context/storeContext";
+import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-  const { food_list, cartItems, removeFromCart, getTotalCartAmount } =
+  const { food_list, cartItems, removeFromCart, getTotalCartAmount, url } =
     useContext(StoreContext);
 
   const navigate = useNavigate();
@@ -27,7 +27,7 @@ const Cart = () => {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
+                  <img src={url + "/images/" + item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
