28770e589293ba04b07c9041fbfa4accf9b24a8c
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-26 17:49:45 +0530

    add logic remove from cart from database with frnend api called from backend

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -24,8 +24,15 @@ const StoreContextProvider = (props) => {
     }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
   const getTotalCartAmount = () => {
