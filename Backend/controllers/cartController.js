8a143e00ac870716e1921b820d8e7d64680d1537
Sarfaraj32 <usemyno12345@gmail.com>
2025-09-26 16:34:06 +0530

    Created AddToCart Functionality

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -1,7 +1,22 @@
 import UserModel from "../models/userModel.js";
 
 // Add items tp User Cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    let userData = await UserModel.findOne({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await UserModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added To Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // Remove items from User Cart
 const removeFromCart = async (req, res) => {};
